7f28e072755e75f53fe3f3a3c2fc8455db5a0c0e
ShakerCod <74952205+ShakerCod@users.noreply.github.com>
2020-11-27 16:52:28 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,69 +1,65 @@
-const Engine=Matter.Engine;
-const World=Matter.World;
-const Bodies=Matter.Bodies;
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var packageBody,ground
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
 
-var engine,world,ground;
-var ball,ball2,ball3,ball4;
+function preload()
+{
+	helicopterIMG=loadImage("helicopter.png")
+	packageIMG=loadImage("package.png")
+}
+
 function setup() {
-  createCanvas(400,400);
+	createCanvas(800, 700);
+	rectMode(CENTER);
+	
 
-  engine=Engine.create();
-  world=engine.world;
+	packageSprite=createSprite(width/2, 80, 10,10);
+	packageSprite.addImage(packageIMG)
+	packageSprite.scale=0.2
 
-  var ground_options={
-    isStatic:true
-  }
+	helicopterSprite=createSprite(width/2, 200, 10,10);
+	helicopterSprite.addImage(helicopterIMG)
+	helicopterSprite.scale=0.6
 
-  var ball_options={
-    restitution:1.0
-  }
-
-
-
-
-
-  ground=Bodies.rectangle(200,390,300,20,ground_options);
-  ball=Bodies.circle(200,100,20,ball_options);
-  ball2=Bodies.circle(300,200,20,ball_options);
-  ball3=Bodies.circle(100,300,20,ball_options);
-  ball4=Bodies.circle(150,250,20,ball_options);
+	groundSprite=createSprite(width/2, height-35, width,10);
+	groundSprite.shapeColor=color(255)
 
 
+	engine = Engine.create();
+	world = engine.world;
 
-  World.add(world,ground);
-  World.add(world,ball);
-  World.add(world,ball2);
-  World.add(world,ball3);
-  World.add(world,ball4);
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.4, isStatic:true});
+	World.add(world, packageBody);
+	
 
-  console.log(ball);
+	//Create a Ground
+	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+ 	World.add(world, ground);
 
-  console.log(ball.position.x);
 
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  packageSprite.x= packageBody.position.x 
+  packageSprite.y= packageBody.position.y 
+  drawSprites();
  
 }
 
-function draw() {
-  background(0,0,0); 
-  Engine.update(engine);
+function keyPressed() {
+ if (keyCode === DOWN_ARROW) {
 
-
-  rectMode(CENTER);
-  rect(ground.position.x,ground.position.y,300,20);  
-
-  ellipseMode(RADIUS);
-  ellipse(ball.position.x,ball.position.y,20,20);
-
-  ellipseMode(RADIUS);
-  ellipse(ball2.position.x,ball2.position.y,10,10);
-
-  ellipseMode(RADIUS);
-  ellipse(ball3.position.x,ball3.position.y,10,10);
-
-  ellipseMode(RADIUS);
-  ellipse(ball4.position.x,ball4.position.y,10,10);
-
-
-
-
+  Matter.Body.setStatic(packageBody,false);  
+  }
 }
+
+
+
